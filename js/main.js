01a556e32f7672b60f6c11a8d719ba39664bca2b
LinoQuirogaMusic <86981122+LinoQuirogaMusic@users.noreply.github.com>
2024-07-22 01:57:49 -0400

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,22 +1,43 @@
 import { auth, provider, firestore } from './firebase-config.js';
 import { signInWithPopup } from 'https://www.gstatic.com/firebasejs/9.0.0/firebase-auth.js';
-import { getDocs, collection, addDoc } from 'https://www.gstatic.com/firebasejs/9.0.0/firebase-firestore.js';
+import { getDocs, collection, addDoc, getDoc, doc } from 'https://www.gstatic.com/firebasejs/9.0.0/firebase-firestore.js';
 
 document.addEventListener('DOMContentLoaded', () => {
     const googleLoginButton = document.getElementById('google-login');
     if (googleLoginButton) {
-        googleLoginButton.addEventListener('click', () => {
-            signInWithPopup(auth, provider)
-                .then((result) => {
-                    const user = result.user;
-                    localStorage.setItem('user_name', user.displayName);
-                    localStorage.setItem('user_id', user.uid);  // Almacenamos el user_id también
-                    localStorage.setItem('id_token', user.accessToken);
-                    window.location.href = 'dashboard.html';
-                })
-                .catch((error) => {
-                    console.error('Error during sign-in:', error);
-                });
+        googleLoginButton.addEventListener('click', async () => {
+            try {
+                const result = await signInWithPopup(auth, provider);
+                const user = result.user;
+                const userId = user.uid;
+
+                // Obtener el documento del usuario desde Firestore
+                const userRef = doc(firestore, 'users', userId);
+                const userDoc = await getDoc(userRef);
+
+                if (userDoc.exists()) {
+                    const userData = userDoc.data();
+                    const userRole = userData.role;
+
+                    // Verificar el rol del usuario
+                    if (userRole === 'doctor') {
+                        localStorage.setItem('user_name', user.displayName);
+                        localStorage.setItem('user_id', user.uid);  // Almacenamos el user_id también
+                        localStorage.setItem('id_token', user.accessToken);
+                        window.location.href = 'dashboard.html';
+                    } else {
+                        alert('No está autorizado para acceder a esta aplicación.');
+                        // Opcional: cerrar sesión
+                        await auth.signOut();
+                    }
+                } else {
+                    alert('Usuario no encontrado en la base de datos.');
+                    // Opcional: cerrar sesión
+                    await auth.signOut();
+                }
+            } catch (error) {
+                console.error('Error during sign-in:', error);
+            }
         });
     }
 });
